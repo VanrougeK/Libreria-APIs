23ce72ae23ffedd1c6e669b8d0da77cffa6f0ff3
VanrougeK <ochoavania795@gmail.com>
2026-07-15 13:37:21 -0600

    Correccion import de middleware en rutas

--- Backend/routes/pdfRoutes.js
+++ Backend/routes/pdfRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { authMiddleware } = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const { getPdfs } = require("../controllers/pdfController");
 
 router.get("/", authMiddleware, getPdfs);
